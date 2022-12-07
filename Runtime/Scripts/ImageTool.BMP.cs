0e5235aba74ecb97c089e4d7a27291d5a18bd833
Sxer_lmh <308457938@qq.com>
2022-12-07 09:26:49 +0800

    创建

---
+++ Runtime/Scripts/ImageTool.BMP.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using UnityEngine;
+
+namespace Sxer.Tool
+{
+    public static partial class ImageTool
+    {
+        public static byte[] BMP2PNG(byte[] bmpdata)
+        {
+            using (MemoryStream stream = new MemoryStream(bmpdata))
+            {
+                Bitmap bitmap;
+                bitmap = new Bitmap(stream);
+                return Bitmap2Byte(bitmap, System.Drawing.Imaging.ImageFormat.Png);
+            }
+        }
+        public static byte[] BMP2JPG(byte[] bmpdata)
+        {
+            using (MemoryStream stream = new MemoryStream(bmpdata))
+            {
+                Bitmap bitmap;
+                bitmap = new Bitmap(stream);
+                return Bitmap2Byte(bitmap, System.Drawing.Imaging.ImageFormat.Jpeg);
+            }
+        }
+
+        private static byte[] Bitmap2Byte(Bitmap bitmap, System.Drawing.Imaging.ImageFormat imageFormat)
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                // 将bitmap 以png格式保存到流中
+                bitmap.Save(stream, imageFormat);
+                // 创建一个字节数组，长度为流的长度
+                byte[] data = new byte[stream.Length];
+                // 重置指针
+                stream.Seek(0, SeekOrigin.Begin);
+                // 从流读取字节块存入data中
+                stream.Read(data, 0, System.Convert.ToInt32(stream.Length));
+                return data;
+            }
+        }
+
+
+
+    }
+}
+
---
+++ Runtime/Scripts/ImageTool.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace Sxer.Tool
+{
+    public static partial class ImageTool
+    {
+        public const string Head2Byte_PNG = "13780";
+        public const string Head2Byte_JPG = "255216";
+        public const string Head2Byte_BMP = "6677";
+        public const string Head2Byte_GIF = "7173";
+
+
+        /// <summary>
+        /// 检测当前图片文件格式
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        public static ImageFileExtension CheckImageFileExtension(string fileName)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            BinaryReader br = new BinaryReader(fs);
+            string fileType = string.Empty;
+            try
+            {
+                byte data = br.ReadByte();
+                fileType += data.ToString();
+                data = br.ReadByte();
+                fileType += data.ToString();
+              
+                return GetImageType(fileType);
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (fs != null)
+                {
+                    br.Close();
+                    fs.Close();
+                }
+            }
+        }
+
+        public static ImageFileExtension CheckImageFileExtension(byte[] data)
+        {
+            string fileType = string.Empty;
+            try
+            {
+                fileType = data[0].ToString() + data[1].ToString();
+
+                return GetImageType(fileType);
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        static ImageFileExtension GetImageType(string headbyte)
+        {
+            switch (headbyte)
+            {
+                case Head2Byte_PNG:
+                    return ImageFileExtension.PNG;
+                case Head2Byte_JPG:
+                    return ImageFileExtension.JPG;
+                case Head2Byte_BMP:
+                    return ImageFileExtension.BMP;
+                case Head2Byte_GIF:
+                    return ImageFileExtension.GIF;
+            }
+            return ImageFileExtension.VALIDFILE;
+        }
+
+
+    }
+
+
+   
+
+
+
+    public enum ImageFileExtension
+    {
+        BMP = 6677,
+        JPG = 255216,
+        PNG = 13780,
+        GIF = 7173,
+        VALIDFILE = 9999999
+        //SWF = 6787,
+        //RAR = 8297,
+        //ZIP = 8075,
+        //_7Z = 55122,
+
+        // 255216 jpg; 
+
+        // 7173 gif; 
+
+        // 6677 bmp, 
+
+        // 13780 png; 
+
+        // 6787 swf 
+
+        // 7790 exe dll, 
+
+        // 8297 rar 
+
+        // 8075 zip 
+
+        // 55122 7z 
+
+        // 6063 xml 
+
+        // 6033 html 
+
+        // 239187 aspx 
+
+        // 117115 cs 
+
+        // 119105 js 
+
+        // 102100 txt 
+
+        // 255254 sql  
+
+    }
+
+}
+
+
