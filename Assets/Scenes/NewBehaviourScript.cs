59233aa9199e5cfea91c5ef191eae6c59e311d0e
Sxer_lmh <308457938@qq.com>
2022-12-07 09:26:49 +0800

    创建

---
+++ Assets/Scenes/NewBehaviourScript.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sxer.Tool;
+using System.IO;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    Texture2D tex;
+    int len;
+    int nowlen = 0;
+    byte[] pngbyte;
+    byte[] gifByte;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(
+        ImageTool.CheckImageFileExtension(@"C: \Users\DS\Desktop\郊庙歌辞 晋昭德成\郊庙歌辞 晋昭德成.jpg")
+        );
+
+        Debug.Log(
+        ImageTool.CheckImageFileExtension(@"C:\Users\DS\Desktop\6.4.png")
+        );
+
+        pngbyte = ImageTool.BMP2JPG(File.ReadAllBytes(@"C: \Users\DS\Desktop\郊庙歌辞 晋昭德成\郊庙歌辞 晋昭德成.jpg"));
+        Debug.Log(ImageTool.CheckImageFileExtension(pngbyte));
+
+
+        //Debug.Log(ImageTool.CheckImageFileExtension(@"C: \Users\DS\Desktop\182c8cf2525_b48.gif"));
+        //gifByte = File.ReadAllBytes(@"C: \Users\DS\Desktop\182c8cf2525_b48.gif");
+        //len = ImageTool.GetGifLength(gifByte);
+
+        //pngbyte = ImageTool.GetGifFrameIndex_JPG(gifByte, nowlen);
+
+
+
+
+        tex = new Texture2D(512, 512);
+        tex.LoadImage(pngbyte);
+
+
+
+
+        ImageTool.ScaleTexture(tex,10,10);
+       // ImageTool.ScaleTexture()
+
+
+    }
+
+
+    private void OnGUI()
+    {
+        GUI.DrawTexture(new Rect(200, 200, 200, 200), tex);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //nowlen++;
+        //if (nowlen >= len)
+        //{
+        //    nowlen = 0;
+        //}
+        //pngbyte = ImageTool.GetGifFrameIndex_JPG(gifByte, nowlen);
+        //tex.LoadImage(pngbyte);
+    }
+}
---
+++ Assets/com.sxer.tool.imagetool/Runtime/Scripts/ImageTool.GIF.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using UnityEngine;
+
+namespace Sxer.Tool
+{
+    public static partial class ImageTool
+    {
+        public static int GetGifLength(byte[] gifdata)
+        {
+            using (MemoryStream stream = new MemoryStream(gifdata))
+            {
+                using (Image gifImage = Image.FromStream(stream))
+                {
+                    FrameDimension frame = new System.Drawing.Imaging.FrameDimension(gifImage.FrameDimensionsList[0]);
+                    Debug.Log("gif帧数:" + gifImage.GetFrameCount(frame));
+                    return gifImage.GetFrameCount(frame);
+                }
+            }
+        }
+
+
+        public static byte[] GetGifFrameIndex_PNG(byte[] gifdata,int frameIndex)
+        {
+            using (MemoryStream stream = new MemoryStream(gifdata))
+            {
+                using (Image gifImage = Image.FromStream(stream))
+                {
+                    FrameDimension frame = new System.Drawing.Imaging.FrameDimension(gifImage.FrameDimensionsList[0]);
+                    if (frameIndex < gifImage.GetFrameCount(frame))
+                    {
+                        gifImage.SelectActiveFrame(frame, frameIndex);
+                        Bitmap framBitmap = new Bitmap(gifImage.Width, gifImage.Height);
+                        using (System.Drawing.Graphics graphic = System.Drawing.Graphics.FromImage(framBitmap))
+                        {
+                            graphic.DrawImage(gifImage, System.Drawing.Point.Empty);
+                        }
+
+                        byte[] useByte = Bitmap2Byte(framBitmap, ImageFormat.Png);
+
+                        frame = null;
+                        framBitmap.Dispose();
+                        framBitmap = null;
+
+                        return useByte;
+                    }
+                    return null;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 获取gif的第几帧的图像
+        /// </summary>
+        /// <param name="gifdata">gif图片数据</param>
+        /// <param name="frameIndex">帧数</param>
+        /// <returns>jpg数据</returns>
+        public static byte[] GetGifFrameIndex_JPG(byte[] gifdata, int frameIndex)
+        {
+            using (MemoryStream stream = new MemoryStream(gifdata))
+            {
+                using (Image gifImage = Image.FromStream(stream))
+                {
+                    FrameDimension frame = new System.Drawing.Imaging.FrameDimension(gifImage.FrameDimensionsList[0]);
+                    if (frameIndex < gifImage.GetFrameCount(frame))
+                    {
+                        gifImage.SelectActiveFrame(frame, frameIndex);
+                        Bitmap framBitmap = new Bitmap(gifImage.Width, gifImage.Height);
+                        using (System.Drawing.Graphics graphic = System.Drawing.Graphics.FromImage(framBitmap))
+                        {
+                            graphic.DrawImage(gifImage, System.Drawing.Point.Empty);
+                        }
+
+                        byte[] useByte = Bitmap2Byte(framBitmap, ImageFormat.Jpeg);
+
+                        frame = null;
+                        framBitmap.Dispose();
+                        framBitmap = null;
+
+                        return useByte;
+                    }
+                    return null;
+                }
+            }
+        }
+
+
+    }
+}
+
